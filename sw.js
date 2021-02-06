a19efaa5081a3ed18fd2886f6b9cc8de383bed99
mabalashov <mabalashov.home@gmail.com>
2021-02-06 19:43:25 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,26 +27,26 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4e9da526be39ede7e098.js"
+    "url": "webpack-runtime-0ee95f8f77dbc653c8be.js"
   },
   {
-    "url": "framework-a38040c4749cf445737f.js"
+    "url": "framework-a4620de0399b10c30110.js"
   },
   {
-    "url": "app-d1173eb960369c776214.js"
+    "url": "app-ef3e1b90ab2fcd95f2fd.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "84a0f029d8e9bfe73540ca592b85a510"
+    "revision": "7ced362c9b758d89910fe054c2a0647c"
   },
   {
-    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e5cb9e0c77a823b86dc2.js"
+    "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5a1ee105f05b3144b807.js"
   },
   {
-    "url": "polyfill-68528c3be76e45e09e78.js"
+    "url": "polyfill-78da944167c13338ef4c.js"
   },
   {
-    "url": "styles.22ec855d56f3587fc08b.css"
+    "url": "styles.380d43fa49a0c1428af4.css"
   },
   {
     "url": "styles-c2fe8482057191dca484.js"
@@ -55,7 +55,7 @@ self.__precacheManifest = [
     "url": "d3a78f1f547d688dafab540ea9def2e743b23750-72b3a6e0ad2146c99148.js"
   },
   {
-    "url": "component---src-templates-tags-list-template-js-e95bbfdf0bf1fa651710.js"
+    "url": "component---src-templates-tags-list-template-js-81dcce268a871d47d5d7.js"
   },
   {
     "url": "page-data/tags/page-data.json",
@@ -75,31 +75,34 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "930a2e9a6e70ed06feff0eeec95d3a65"
+    "revision": "6447651ae3746add948b121c13814188"
   },
   {
-    "url": "component---src-templates-categories-list-template-js-e4de52e83278652e27c5.js"
+    "url": "component---src-templates-categories-list-template-js-c99eca7fb9655967d6d2.js"
   },
   {
     "url": "page-data/categories/page-data.json",
     "revision": "b3dfa4c91efcf3685664f836c9b685ab"
   },
   {
-    "url": "component---src-templates-cv-template-js-719c7664bda467cd0bc1.js"
+    "url": "44934075e77211be33e4e2ec44abb32cb826714c-bfc0e22679f5bcf65c87.js"
+  },
+  {
+    "url": "component---src-templates-cv-template-js-54133272e5582371be8f.js"
   },
   {
     "url": "page-data/cv/page-data.json",
     "revision": "9a1181f38e25168c1024a39d6efea6a9"
   },
   {
-    "url": "component---src-templates-not-found-template-js-27d377dc28e43d2fc11d.js"
+    "url": "component---src-templates-not-found-template-js-d60627b91340f73de727.js"
   },
   {
     "url": "page-data/404/page-data.json",
     "revision": "86aa696998e7c04d0cb298aa4f3f4eb9"
   },
   {
-    "url": "component---src-templates-post-template-js-aaf90dda43d385957012.js"
+    "url": "component---src-templates-post-template-js-c0b8b35f450383dc5c84.js"
   },
   {
     "url": "page-data/posts/bounded-context/page-data.json",
@@ -127,22 +130,22 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/posts/implementing-solid-and-onion-architecture-nodejs-typescript-and-inversifyjs/page-data.json",
-    "revision": "45daabd335c2ab01d919d84821edf327"
+    "revision": "33318dff6347e6968fc6d882a6f49937"
   },
   {
     "url": "page-data/posts/mysql-index-typecast-issue/page-data.json",
-    "revision": "aeaf1183d09dfbca9842c4f3afae1644"
+    "revision": "a1151bef5526d3d48ec92d525fda5812"
   },
   {
     "url": "page-data/posts/symfony-messenger-in-laravel/page-data.json",
-    "revision": "385434ce5e8aa38c3fd98288afa66d28"
+    "revision": "3733d589cfb3f6ff4d114c43535375c7"
   },
   {
     "url": "page-data/posts/the-clean-architecture/page-data.json",
     "revision": "61542957b4b0ff9b1bd7fa8fb28b29ce"
   },
   {
-    "url": "component---src-templates-page-template-js-2e67a5152b3285a2afd5.js"
+    "url": "component---src-templates-page-template-js-bdfc6c6141b848822b93.js"
   },
   {
     "url": "page-data/pages/as-privacy-policy/page-data.json",
@@ -242,7 +245,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-d1173eb960369c776214.js`))) {
+  if (!resources || !(await caches.match(`/app-ef3e1b90ab2fcd95f2fd.js`))) {
     return await fetch(event.request)
   }
 
